e5380461ecea8ec331befe937b99fca217406f14
Edoardo Debenedetti <edoardo.debenedetti@epfl.ch>
2022-02-02 20:09:52 +0100

    Add validation script that uses robustbench

---
+++ validate_robustbench.py
@@ -0,0 +1,65 @@
+import argparse
+
+import torch
+
+import models
+import utils
+from robustbench import benchmark
+from robustbench.model_zoo.enums import BenchmarkDataset, ThreatModel
+
+from validate import log_results_to_wandb
+
+parser = argparse.ArgumentParser(description='Validation using RobustBench')
+parser.add_argument('--patch-size', default=16, type=int, metavar='N', help='The patch size to use')
+parser.add_argument('--dataset', default="cifar10", type=str, metavar='DATASET')
+parser.add_argument('--data-dir', default="~/torch_data/", type=str, metavar='DATASET')
+parser.add_argument('--checkpoint', default=None, type=str, metavar='PATH')
+parser.add_argument('--model', default=None, type=str, metavar='NAME')
+parser.add_argument('--batch-size', default=512, type=int, metavar='N')
+parser.add_argument('--num-classes', default=10, type=int, metavar='N')
+parser.add_argument('--eps', default=8, type=int)
+parser.add_argument('--log-wandb',
+                    action='store_true',
+                    default=False,
+                    help='Log results to wandb using the run stored in the bucket')
+
+
+def main(args):
+    model = utils.load_model_from_gcs(
+        args.checkpoint,
+        args.model,
+        num_classes=args.num_classes,
+        in_chans=3,
+    )
+    if model.patch_embed.patch_size != args.patch_size:
+        assert args.patch_size in {4, 8}, "Finetuning patch size can be only 4, 8 or `None`"
+        assert isinstance(model, models.xcit.XCiT), "Finetuning patch size is only supported for XCiT"
+        print(f"Adapting patch embedding for finetuning patch size {args.patch_size}")
+        model.patch_embed.patch_size = args.patch_size
+        model.patch_embed.proj[0][0].stride = (1, 1)
+        if args.patch_size == 4:
+            model.patch_embed.proj[2][0].stride = (1, 1)
+
+    device = torch.device("cuda:0")
+    model.eval()
+    model.to(device)
+
+    clean_acc, robust_acc = benchmark(model,
+                                      dataset=args.dataset,
+                                      data_dir=args.data_dir,
+                                      device=device,
+                                      batch_size=args.batch_size,
+                                      eps=args.eps / 255,
+                                      preprocessing='Res256Crop224')
+
+    if args.log_wandb:
+        args.attack_eps = args.eps / 255
+        args.attack_steps = None
+        args.attack = "autoattack"
+        results_dict = {'top1': clean_acc, 'robust_top1': robust_acc}
+        log_results_to_wandb(args, results_dict)
+
+
+if __name__ == "__main__":
+    _args = parser.parse_args()
+    main(_args)
