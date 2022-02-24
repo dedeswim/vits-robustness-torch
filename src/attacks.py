aaf79ce0a9892bf9f957c0d3e87d5cbeaad94649
Edoardo Debenedetti <edoardo.debenedetti@epfl.ch>
2022-02-24 11:39:25 +0000

    Fix error in attacks.py file

--- src/attacks.py
+++ src/attacks.py
@@ -1,4 +1,5 @@
 import functools
+import math
 from typing import Callable, Dict, Optional, Tuple
 
 import torch
@@ -72,6 +73,71 @@ _ATTACKS = {"pgd": pgd, "targeted_pgd":
 _INIT_PROJECT_FN: Dict[str, Tuple[InitFn, ProjectFn]] = {"linf": (init_linf, project_linf)}
 
 
+def make_sine_schedule(final: float, warmup: int, zero_eps_epochs: int) -> Callable[[int], float]:
+    def sine_schedule(step: int) -> float:
+        if step < zero_eps_epochs:
+            return 0.0
+        if step < warmup:
+            return 0.5 * final * (1 + math.sin(math.pi * ((step - zero_eps_epochs) / warmup - 0.5)))
+        return final
+
+    return sine_schedule
+
+
+def make_linear_schedule(final: float, warmup: int, zero_eps_epochs: int) -> Callable[[int], float]:
+    def linear_schedule(step: int) -> float:
+        if step < zero_eps_epochs:
+            return 0.0
+        if step < warmup:
+            return (step - zero_eps_epochs) / warmup * final
+        return final
+
+    return linear_schedule
+
+
+_SCHEDULES: Dict[str, ScheduleMaker] = {
+    "linear": make_linear_schedule,
+    "sine": make_sine_schedule,
+    "constant": (lambda eps, _1, _2: (lambda _: eps))
+}
+
+
+def make_train_attack(attack_name: str, schedule: str, final_eps: float, period: int, zero_eps_epochs: int,
+                      step_size: float, steps: int, norm: Norm, boundaries: Tuple[float, float],
+                      criterion: nn.Module, num_classes: int, logits_y: bool) -> TrainAttackFn:
+    if attack_name in {"ll", "soft-labels"}:
+        attack_mode: Optional[str] = attack_name
+        attack_name = "pgd"
+    else:
+        attack_mode = None
+    attack_fn = _ATTACKS[attack_name]
+    init_fn, project_fn = _INIT_PROJECT_FN[norm]
+    schedule_fn = _SCHEDULES[schedule](final_eps, period, zero_eps_epochs)
+
+    def attack(model: nn.Module, x: torch.Tensor, y: torch.Tensor, step: int) -> torch.Tensor:
+        eps = schedule_fn(step)
+        if attack_mode == "ll":
+            with torch.no_grad():
+                y = model(x).argmin(dim=-1)
+        elif attack_mode == "soft-labels":
+            with torch.no_grad():
+                y = model(x)
+        return attack_fn(model,
+                         x,
+                         y,
+                         eps,
+                         step_size=step_size,
+                         steps=steps,
+                         boundaries=boundaries,
+                         init_fn=init_fn,
+                         project_fn=project_fn,
+                         criterion=criterion,
+                         num_classes=num_classes,
+                         logits_y=logits_y)
+
+    return attack
+
+
 def make_attack(attack: str,
                 eps: float,
                 step_size: float,
@@ -100,3 +166,41 @@ def make_attack(attack: str,
         return x_adv  # type: ignore
 
     return autoattack_fn
+
+
+class AdvTrainingLoss(nn.Module):
+    def __init__(self, attack: TrainAttackFn, criterion: nn.Module):
+        super().__init__()
+        self.attack = attack
+        self.criterion = criterion
+
+    def forward(self, model: nn.Module, x: torch.Tensor, y: torch.Tensor,
+                epoch: int) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        x_adv = self.attack(model, x, y, epoch)
+        logits, logits_adv = model(x), model(x_adv)
+        loss = self.criterion(logits_adv, y)
+        return loss, logits, logits_adv
+
+
+class TRADESLoss(nn.Module):
+    def __init__(self, attack: TrainAttackFn, natural_criterion: nn.Module, beta: float):
+        super().__init__()
+        self.attack = attack
+        self.natural_criterion = natural_criterion
+        self.kl_criterion = nn.KLDivLoss(reduction="sum")
+        self.beta = beta
+
+    def forward(self, model: nn.Module, x: torch.Tensor, y: torch.Tensor,
+                epoch: int) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        batch_size = x.size(0)
+        # model.eval()  # FIXME: understand why with eval the gradient
+        # of BatchNorm crashes
+        output_softmax = F.softmax(model(x.detach()), dim=-1)
+        x_adv = self.attack(model, x, output_softmax, epoch)
+        model.train()
+        logits, logits_adv = model(x), model(x_adv)
+        loss_natural = self.natural_criterion(logits, y)
+        loss_robust = (1.0 / batch_size) * self.kl_criterion(F.log_softmax(logits_adv, dim=1),
+                                                             F.softmax(logits, dim=1))
+        loss = loss_natural + self.beta * loss_robust
+        return loss, logits, logits_adv
