371dc62f37e4b9b66ca03e9c31431fe83a512d09
Vikash Sehwag <vvikash@princeton.edu>
2022-03-13 12:40:48 -0400

    log ablation on wandb

--- ablation_gpu.sh
+++ ablation_gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ->->->->->-> select key variables based on princeton or epfl <-<-<-<-<-<-
-cluster="ep" # switch between pn/ep
+cluster="pn" # switch between pn/ep
 
 if [ $cluster == "pn" ] 
 then
@@ -31,7 +31,7 @@ then
     DATASET="--dataset torch/cifar10 --num-classes 10"
     NORMALIZATION="--mean 0.4914 0.4822 0.4465 --std 0.2471 0.2435 0.2616 --normalize-model"
     ATTACK="--attack-steps 10 --attack-eps 8 --adv-training trades"
-    SETUP="--config configs/xcit-adv-finetuning.yaml --finetune ${CKPTDIR} --output ${SAVEDIR} --sync-bn"
+    SETUP="--config configs/xcit-adv-finetuning.yaml --finetune ${CKPTDIR}/xcit_s_eps_8.pth.tar --output ${SAVEDIR} --sync-bn --log-wandb"
     
     for wd in 0.05 0.5 1.0
     do 
@@ -41,20 +41,23 @@ then
         lr=0.00005
         CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=6712 train.py \
             $DATADIR $DATASET $SETUP $NORMALIZATION $ATTACK $HYPERPARAMS --batch-size 64 --lr $lr --trades-beta ${beta} \
-            --experiment "xcit-adv-finetuning-gpu_cifar10_ablations_lr_${lr}_wd_${wd}_beta_${beta}" &
+            --experiment "xcit-adv-finetuning-gpu_cifar10_ablations_only_randErasing" &
+        sleep 1
         lr=0.0001
         CUDA_VISIBLE_DEVICES=2,3 python -m torch.distributed.launch --nproc_per_node=2 --master_port=6713 train.py \
             $DATADIR $DATASET $SETUP $NORMALIZATION $ATTACK $HYPERPARAMS --batch-size 64 --lr $lr --trades-beta ${beta} \
-            --experiment "xcit-adv-finetuning-gpu_cifar10_ablations_lr_${lr}_wd_${wd}_beta_${beta}" &
+            --experiment "xcit-adv-finetuning-gpu_cifar10_ablations_only_randErasing" &
+        sleep 1
         beta=12.0
         lr=0.00005
         CUDA_VISIBLE_DEVICES=4,5 python -m torch.distributed.launch --nproc_per_node=2 --master_port=6714 train.py \
             $DATADIR $DATASET $SETUP $NORMALIZATION $ATTACK $HYPERPARAMS --batch-size 64 --lr $lr --trades-beta ${beta} \
-            --experiment "xcit-adv-finetuning-gpu_cifar10_ablations_lr_${lr}_wd_${wd}_beta_${beta}" &
+            --experiment "xcit-adv-finetuning-gpu_cifar10_ablations_only_randErasing" &
+        sleep 1
         lr=0.0001
         CUDA_VISIBLE_DEVICES=6,7 python -m torch.distributed.launch --nproc_per_node=2 --master_port=6715 train.py \
             $DATADIR $DATASET $SETUP $NORMALIZATION $ATTACK $HYPERPARAMS --batch-size 64 --lr $lr --trades-beta ${beta} \
-            --experiment "xcit-adv-finetuning-gpu_cifar10_ablations_lr_${lr}_wd_${wd}_beta_${beta}" ;
+            --experiment "xcit-adv-finetuning-gpu_cifar10_ablations_only_randErasing" ;
         wait;
     done
 fi
@@ -65,7 +68,7 @@ then
     DATASET="--dataset torch/cifar100 --num-classes 100"
     NORMALIZATION="--mean 0.5071 0.4867 0.4408 --std 0.2675 0.2565 0.2761 --normalize-model"
     ATTACK="--attack-steps 10 --attack-eps 8 --adv-training trades"
-    SETUP="--config configs/xcit-adv-finetuning.yaml --finetune ${CKPTDIR}/xcit_s_eps_8.pth.tar --output ${SAVEDIR} --sync-bn"
+    SETUP="--config configs/xcit-adv-finetuning.yaml --finetune ${CKPTDIR}/xcit_s_eps_8.pth.tar --output ${SAVEDIR} --sync-bn --log-wandb"
     
     for beta in 6 12
     do 
@@ -73,14 +76,16 @@ then
         do 
             HYPERPARAMS="--epochs 20 --color-jitter 0.0 --cutmix 0.0 --reprob 0.20 --weight-decay ${wd} --smoothing 0.0 \
                 --cooldown-epoch 2  --trades-beta ${beta}"
+            echo $SETUP
             lr=0.00005
             CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=6713 train.py \
                 $DATADIR $DATASET $SETUP $NORMALIZATION $ATTACK $HYPERPARAMS --batch-size 64 --lr $lr \
-                --experiment "xcit-adv-finetuning-gpu_cifar100_ablations_lr_${lr}_wd_${wd}_beta_${beta}_only_randErasing" &
+                --experiment "xcit-adv-finetuning-gpu_cifar100_ablations_only_randErasing" &
+            sleep 1
             lr=0.0001
             CUDA_VISIBLE_DEVICES=2,3 python -m torch.distributed.launch --nproc_per_node=2 --master_port=6714 train.py \
-                $DATADIR $DATASET $SETUP $NORMALIZATION $ATTACK $HYPERPARAMS --batch-size 64 --lr $lr 
-                --experiment "xcit-adv-finetuning-gpu_cifar100_ablations_lr_${lr}_wd_${wd}_beta_${beta}_only_randErasing" ;
+                $DATADIR $DATASET $SETUP $NORMALIZATION $ATTACK $HYPERPARAMS --batch-size 64 --lr $lr \
+                --experiment "xcit-adv-finetuning-gpu_cifar100_ablations_only_randErasing" ;
             wait;
         done
     done
