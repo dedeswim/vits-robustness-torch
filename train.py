bba96ae250a27c385684394c247f8aab224a8c20
Edoardo Debenedetti <edoardo.m.debenedetti@gmail.com>
2022-03-14 22:49:36 +0000

    Fix issue with new models structure

--- train.py
+++ train.py
@@ -24,6 +24,7 @@ from datetime import datetime
 from typing import Optional, Tuple
 
 import tensorflow as tf
+import timm
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -334,11 +335,11 @@ def setup_train_task(args, dev_env: DeviceEnv, mixup_active: bool):
 
     if args.finetuning_patch_size is not None:
         assert args.finetuning_patch_size in {2, 4, 8}, "Finetuning patch size can be only 4, 8 or `None`"
-        assert isinstance(model, models.xcit.XCiT), "Finetuning patch size is only supported for XCiT"
+        assert isinstance(model, timm.models.xcit.XCiT), "Finetuning patch size is only supported for XCiT"
         _logger.info(f"Adapting patch embedding for finetuning patch size {args.finetuning_patch_size}")
         model.patch_embed.patch_size = args.finetuning_patch_size
 
-        # FIXME use models from `models` module 
+        # FIXME use models from `models` module
         if args.keep_patch_embedding:
             model.patch_embed.proj[0][0].stride = (1, 1)
             if args.finetuning_patch_size == 4:
@@ -350,15 +351,16 @@ def setup_train_task(args, dev_env: DeviceEnv, mixup_active: bool):
         else:
             if args.finetuning_patch_size == 4:
                 model.patch_embed.proj = model.patch_embed.proj[-3:]
-                model.patch_embed.proj[0] = models.xcit.conv3x3(3, model.embed_dim // 2, 2)
+                model.patch_embed.proj[0] = timm.models.xcit.conv3x3(3, model.embed_dim // 2, 2)
 
             elif args.finetuning_patch_size == 8:
                 if args.reinit_patch_embedding:
                     _logger.info("Re-initializing patch embedding")
-                    model.patch_embed = models.xcit.ConvPatchEmbed(args.input_size[-1], 8, 3, model.embed_dim)
+                    model.patch_embed = timm.models.xcit.ConvPatchEmbed(args.input_size[-1], 8, 3,
+                                                                        model.embed_dim)
                 else:
                     model.patch_embed.proj = model.patch_embed.proj[-5:]
-                    model.patch_embed.proj[0] = models.xcit.conv3x3(3, model.embed_dim // 4, 2)
+                    model.patch_embed.proj[0] = timm.models.xcit.conv3x3(3, model.embed_dim // 4, 2)
 
     if args.num_classes is None:
         assert hasattr(model,
