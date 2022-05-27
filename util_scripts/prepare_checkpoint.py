1f8fe1304e42e17eda665ab8a171dabe5ddaacc4
Edoardo Debenedetti <edoardo.m.debenedetti@gmail.com>
2022-05-27 21:03:47 +0200

    Move utility scripts to a unique folder

--- util_scripts/prepare_checkpoint.py
+++ util_scripts/prepare_checkpoint.py
@@ -8,7 +8,7 @@ import torch
 
 def main(output_dir, model_name, checkpoint_path, dataset, eps=None):
     print(model_name, dataset, eps)
-    checkpoint_path = f"gs://{output_dir}/best.pth.tar"
+    checkpoint_path = f"gs://{checkpoint_path}/best.pth.tar"
     with tempfile.TemporaryDirectory() as dst:
         local_checkpoint_path = os.path.join(dst, os.path.basename(checkpoint_path))
         tf.io.gfile.copy(checkpoint_path, local_checkpoint_path)
@@ -26,21 +26,21 @@ def main(output_dir, model_name, checkpoint_path, dataset, eps=None):
 if __name__ == "__main__":
     models_to_save = [
         # ImageNet eps 4
-        ("robust-vits/xcit", "xcit-s12", "ImageNet", 4),
-        ("robust-vits/xcit-big-5", "xcit-m12", "ImageNet", 4),
-        ("robust-vits-us/xcit-big-50", "xcit-l12", "ImageNet", 4),
-        ("robust-vits-us/model-smoothness-4", "resnet50_gelu", "ImageNet", 4),
-        ("robust-vits-us/convnext-adv-training-1", "convnext_tiny", "ImageNet", 4),
-        # ImageNet eps 8
-        ("robust-vits/xcit-adv-pretraining-3","xcit-s12", "ImageNet", 8),
-        ("robust-vits-us/xcit-adv-pretraining-1", "xcit-m12", "ImageNet", 8),
-        ("robust-vits-us/xcit-adv-pretraining-5", "xcit-l12", "ImageNet", 8),
-        ("robust-vits-us/xcit-adv-pretraining-9", "resnet50_gelu", "ImageNet", 8),
-        ("robust-vits-us/xcit-adv-pretraining-10", "convnext_tiny", "ImageNet", 8),
+        # ("robust-vits/xcit", "xcit-s12", "ImageNet", 4),
+        # ("robust-vits/xcit-big-5", "xcit-m12", "ImageNet", 4),
+        # ("robust-vits-us/xcit-big-50", "xcit-l12", "ImageNet", 4),
+        # ("robust-vits-us/model-smoothness-4", "resnet50_gelu", "ImageNet", 4),
+        # ("robust-vits-us/convnext-adv-training-1", "convnext_tiny", "ImageNet", 4),
+        # # ImageNet eps 8
+        # ("robust-vits/xcit-adv-pretraining-3","xcit-s12", "ImageNet", 8),
+        # ("robust-vits-us/xcit-adv-pretraining-1", "xcit-m12", "ImageNet", 8),
+        # ("robust-vits-us/xcit-adv-pretraining-5", "xcit-l12", "ImageNet", 8),
+        # ("robust-vits-us/xcit-adv-pretraining-9", "resnet50_gelu", "ImageNet", 8),
+        # ("robust-vits-us/xcit-adv-pretraining-10", "convnext_tiny", "ImageNet", 8),
         # CIFAR-10
         ("robust-vits/xcit-adv-finetuning-tpu_cifar10_ablations_only_randErasing-1", "xcit-s12", "CIFAR-10", None),
         ("robust-vits/xcit-adv-finetuning-tpu_cifar10_large-6", "xcit-m12", "CIFAR-10", None),
-        ("robust-vits-us/xcit-adv-finetuning-tpu_cifar10_large-3", "xcit-m12", "CIFAR-10", None),
+        ("robust-vits-us/xcit-adv-finetuning-tpu_cifar10_large-3", "xcit-l12", "CIFAR-10", None),
         ("robust-vits-us/xcit-adv-finetuning-tpu_cifar10_large-22", "resnet_50", "CIFAR-10", None),
         # CIFAR-100
         ("robust-vits-us/xcit-adv-finetuning-tpu_cifar100_large-5", "xcit-s12", "CIFAR-100", None),
